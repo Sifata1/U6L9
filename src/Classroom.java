7d445fefd060b617123ec994e78035201b53936d
BT_4N2_01 <sifata3@nycstudents.net>
2023-01-17 12:31:46 -0500

    Initial commit

---
+++ src/Classroom.java
@@ -0,0 +1,62 @@
+public class Classroom
+{
+    private Student[] classRoster;
+
+    public Classroom(int numStudents)
+    {
+        classRoster = new Student[numStudents];
+    }
+
+    // getter method: returns a reference to classRoster array
+    public Student[] getClassRoster()
+    {
+        return classRoster;
+    }
+
+    /** Adds student to the next available index in classRoster array
+     (when iterating left to right) and return true to indicate
+     student was successfully added.
+     If the array is already maxed out (i.e. the class roster is full),
+     do not add the Student and return false.
+     */
+    public boolean addStudent(Student student)
+    {
+        for (int i = 0; i < classRoster.length; i++) {
+            if (classRoster[i] == null) {
+                classRoster[i] = student;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /** Returns the class exam average determined by adding
+     adding up all exams taken by all students and dividing
+     by total number of exam scores
+     */
+    public double classExamAverage() {
+        double sum = 0;
+        double counter = 0;
+        for (int i = 0; i < classRoster.length; i++) {
+            if (classRoster[i] != null) {
+                for (int x = 0; i < classRoster[i].getExamScores().length; i++) {
+                    {
+                        sum += classRoster[i].getExamScores()[i];
+                        counter++;
+                    }
+                }
+            }
+        }
+        return sum / counter;
+    }
+
+    /** Prints class roster by printing all non-null students
+     in the order they appear in classRoster, one per line,
+     in the format; last name, first name;
+     any null values in classRoster are not printed
+     */
+    public void printRoster()
+    {
+
+    }
+}
